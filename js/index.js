a874e96e98489a8f7e97f59115334c2fae7d4d4e
Thakisis <sub722@gmail.com>
2023-03-19 20:51:21 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+function init(){
+    console.log("init")
+    const ctx=document.getElementById("mycanvas")
+  new window.Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+}
+
+init()
