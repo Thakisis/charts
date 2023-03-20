21218aa55fa2a5ca1dde46c37af95284ccef24e8
Thakisis <sub722@gmail.com>
2023-03-20 01:57:10 +0100

    added css

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,61 @@
 import Chart from 'chart.js/auto';
 
+
+
+
 function init(){
  
-    const ctx=document.getElementById("mycanvas")
-  new Chart(ctx, {
-    type: 'bar',
+
+const grafico1 = document.getElementById("grafico1")
+  new Chart(grafico1, {
+    type: 'doughnut',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['China', 'EEUU', 'Rusia', 'Alemania', 'India', 'Brasil'],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
+        label: 'cantidad Personas',
+        
+        data: [4500000,250000 ,3000000 ,24000000 ,60000000 ,1500000 ],
+        borderWidth: 1,
+        borderColor: '#36A2EB',
+        backgroundColor: [
+        "rgb(0, 123, 177)",
+        "rgb(74, 136, 162)",
+        "rgb(176, 211, 228)",
+        "rgb(250, 172, 130)",
+        "rgb(187, 90, 44)",
+        "rgb(127, 90, 44)"
+      ]
+      
       }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
     }
   });
-}
+  const grafico2 = document.getElementById("grafico2")
+  new Chart(grafico2, {
+    type: 'doughnut',
+    data: {
+      labels: ['China', 'EEUU', 'Rusia', 'Alemania', 'India', 'Brasil'],
+      datasets: [{
+        label: 'cantidad Personas',
+        
+        data: [4500000,250000 ,3000000 ,24000000 ,60000000 ,1500000 ],
+        borderWidth: 1,
+        borderColor: '#36A2EB',
+        backgroundColor: [
+        "rgb(0, 123, 177)",
+        "rgb(74, 136, 162)",
+        "rgb(176, 211, 228)",
+        "rgb(250, 172, 130)",
+        "rgb(187, 90, 44)",
+        "rgb(127, 90, 44)"
+      ]
+      
+      }]
+    }
+  });
+  
+  }
 
-init()
+  init()
+
+
+
